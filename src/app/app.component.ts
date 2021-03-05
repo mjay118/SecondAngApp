8750859a57d1f4df331b3857063774f8f63cd26f
mri118 <mrityunjaysinha0118@gmail.com>
2021-03-05 22:46:00 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'second-app';
+//   title = 'second-app';
+//   a=''
+//   b="xy"
+//   name=''
+//  y=''
+//  x=false;
+//  d="google.com"
+//   ab(){
+//     this.a="ram"+this.b;
+//   }
+//   cd(value: any){
+// this.y="hi "+value;
+//   }
+  emp=[
+    {
+      mob: 13765,
+      age: 22,
+    },
+    {
+      mob: 11,
+      age: 22,
+    },
+    {
+      mob: 33,
+      age: 22,
+    },
+  ]
+// }
+name="angular";
+ date=new Date();
+book = new FormControl(''); 
+author=new FormControl(''); 
+isbn=new FormControl(''); 
 }
